0ddadab03d8049076e5d1d6bee0b8fbb409231f6
oliverbskau <oliverbskau@gmail.com>
2021-10-12 11:09:36 +0200

    færdig´med første version af screening

--- src/main/java/com/itsystem/kino/Model/Screening.java
+++ src/main/java/com/itsystem/kino/Model/Screening.java
@@ -23,12 +23,108 @@ public class Screening {
 
     public Screening(){}
 
+    public int getScreeningID() {
+        return screeningID;
+    }
 
+    public void setScreeningID(int screeningID) {
+        this.screeningID = screeningID;
+    }
 
+    public int getRoomNumber() {
+        return roomNumber;
+    }
 
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
 
+    public String getMovieName() {
+        return movieName;
+    }
 
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
 
+    public Date getStartTime() {
+        return startTime;
+    }
 
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
 
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public double getSeatPrice() {
+        return seatPrice;
+    }
+
+    public void setSeatPrice(double seatPrice) {
+        this.seatPrice = seatPrice;
+    }
+
+    public int getAgeRestriction() {
+        return AgeRestriction;
+    }
+
+    public void setAgeRestriction(int ageRestriction) {
+        AgeRestriction = ageRestriction;
+    }
+
+    public int getReservedSeats() {
+        return reservedSeats;
+    }
+
+    public void setReservedSeats(int reservedSeats) {
+        this.reservedSeats = reservedSeats;
+    }
+
+    public int getAvailableSeats() {
+        return availableSeats;
+    }
+
+    public void setAvailableSeats(int availableSeats) {
+        this.availableSeats = availableSeats;
+    }
+
+    public double getPercentageReserved() {
+        return percentageReserved;
+    }
+
+    public void setPercentageReserved(double percentageReserved) {
+        this.percentageReserved = percentageReserved;
+    }
+
+    public boolean isCancelled() {
+        return isCancelled;
+    }
+
+    public void setCancelled(boolean cancelled) {
+        isCancelled = cancelled;
+    }
+
+    @Override
+    public String toString() {
+        return "Screening{" +
+                "screeningID=" + screeningID +
+                ", roomNumber=" + roomNumber +
+                ", movieName='" + movieName + '\'' +
+                ", startTime=" + startTime +
+                ", endTime=" + endTime +
+                ", seatPrice=" + seatPrice +
+                ", AgeRestriction=" + AgeRestriction +
+                ", reservedSeats=" + reservedSeats +
+                ", availableSeats=" + availableSeats +
+                ", percentageReserved=" + percentageReserved +
+                ", isCancelled=" + isCancelled +
+                '}';
+    }
 }
