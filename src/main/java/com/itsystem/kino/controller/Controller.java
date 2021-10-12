dbad9934fd643fd692128a8bc63e2c9b7e74feac
Raskoe99 <r.koefoed@hotmail.com>
2021-10-12 11:39:22 +0200

    tilføjet Controller

---
+++ src/main/java/com/itsystem/kino/controller/Controller.java
@@ -0,0 +1,16 @@
+package com.itsystem.kino.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @GetMapping ("/")
+    public String Index() {
+
+        return "index.html";
+    }
+
+
+}
