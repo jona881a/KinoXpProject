bbcd26ce0c3c7e8ef077851024700fa6aaf5b6ca
jona881a <jonaskunert@hotmail.com>
2021-10-12 12:23:53 +0200

    Oprettet RestControllerMovies så vi kan fetch movies fra databasen til frontend

--- src/main/java/com/itsystem/kino/Repository/MovieRepository.java
+++ src/main/java/com/itsystem/kino/Repository/MovieRepository.java
@@ -3,5 +3,5 @@ package com.itsystem.kino.Repository;
 import com.itsystem.kino.Model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MoviesRepository extends JpaRepository<Movie, Integer> {
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
 }
