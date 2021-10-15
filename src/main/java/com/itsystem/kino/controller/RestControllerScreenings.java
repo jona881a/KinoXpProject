70bf025360c152b9938c4e186c8d96e3296cd759
jona881a <jonaskunert@hotmail.com>
2021-10-15 12:46:29 +0200

    DET VIRKER!!

--- src/main/java/com/itsystem/kino/controller/RestControllerScreenings.java
+++ src/main/java/com/itsystem/kino/controller/RestControllerScreenings.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(allowedHeaders = "Access-Control-Allow-Origin")
+@CrossOrigin("*")
 @RestController
 public class RestControllerScreenings {
 
