914c19299b8dd8ba906ea5c0b5c21b6d52c7e6d7
Raskoe99 <r.koefoed@hotmail.com>
2021-10-19 11:06:30 +0200

    History er nu en samling af de screenings der har overskredet

--- src/main/java/com/itsystem/kino/controller/RestControllerHistory.java
+++ src/main/java/com/itsystem/kino/controller/RestControllerHistory.java
@@ -1,12 +1,16 @@
 package com.itsystem.kino.controller;
 
 import com.itsystem.kino.model.History;
+import com.itsystem.kino.model.Screening;
 import com.itsystem.kino.repository.HistoryRepository;
+import com.itsystem.kino.repository.ScreeningRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @CrossOrigin(allowedHeaders = "Access-Control-Allow-Origin")
@@ -14,11 +18,22 @@ import java.util.List;
 public class RestControllerHistory {
 
     @Autowired
-    HistoryRepository historyRepository;
+    ScreeningRepository historyRepository;
 
     @GetMapping("/histories")
-    public List<History> getAllHistory(){
-        return historyRepository.findAll();
+    public List<Screening> getAllHistory(){
+        List<Screening> dateScreening = historyRepository.findAll();
+        List<Screening> newScreenings = new ArrayList<>();
+
+        for (Screening scr : dateScreening) {
+            Date dt = scr.getEndTime();
+            Date today = new Date();
+            int compared = dt.compareTo(today);
+            if (compared < 0) {
+                newScreenings.add(scr);
+            }
+        }
+        return newScreenings;
     }
 
 }
